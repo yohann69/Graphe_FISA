f874cf5d75e8b0ae7e6e66d6fac897bb8de29ced
Gilles Simonin <gilles.simonin@imt-atlantique.fr>
2025-03-14 14:32:17 +0100

    Add files via upload

--- FiT_graph_student/src/main/java/Nodes_Edges/DirectedNode.java
+++ FiT_graph_student/src/main/java/Nodes_Edges/DirectedNode.java
@@ -12,10 +12,6 @@ public class DirectedNode extends AbstractNode {
     // 				Class variables
     //--------------------------------------------------
 
-    //private Map<DirectedNode, Integer> succs;
-    //private Map<DirectedNode, Integer> preds;
-	//private List<DirectedNode> succs;
-	//private List<DirectedNode> preds;
 	private List<Arc> arcSucc; // List of out-going arc from the node this.
 	private List<Arc> arcPred; // List of in-going arc from the node this.
 
@@ -25,8 +21,6 @@ public class DirectedNode extends AbstractNode {
 
     public DirectedNode(int i) {
         super(i);
-      //  this.succs = new LinkedList<DirectedNode>();
-      //  this.preds = new LinkedList<DirectedNode>();
         this.arcSucc = new LinkedList<Arc>();
         this.arcPred = new LinkedList<Arc>();
     }
@@ -35,20 +29,6 @@ public class DirectedNode extends AbstractNode {
     // ------------------------------------------
     // 				Accessors
     // ------------------------------------------
-
-    /**
-     * @return the list of successors of the current node this
-     */
-   /* public List<DirectedNode> getSuccs() {
-        return this.succs;
-    }*/
-
-    /**
-     * @return the list of predecessors of the current node this
-     */
-    /*public List<DirectedNode> getPreds() {
-        return preds;
-    }*/
 
     /**
      * @return the number of successors of node this
@@ -80,54 +60,17 @@ public class DirectedNode extends AbstractNode {
 	}
     		
 	
-    /**
-     * @param suc the new list of successors for node this
-     */
-    /*public void setSuccs(List<DirectedNode> suc) {
-        this.succs = suc;
-    }*/
-
-    /**
-     * @param pre the new list of predecessors for nodes this
-     */
-    /*public void setPreds(List<DirectedNode> pre) {
-        this.preds = pre;
-    }*/
-
-
     // ------------------------------------------
     // 				Methods
     // ------------------------------------------
     
-    
-      
-    /**
-	 * add a new successor if it does not exist already.
-	 */
-	/*public void addSucc(DirectedNode v) {
-		if(!this.succs.contains(v)) {
-			this.succs.add(v);
-		}
-	}*/
-   
-	/**
-	 * add a new predecessor if it does not exist already.
-	 */
-	/*public void addPred(DirectedNode v) {
-		if(!this.preds.contains(v)) {
-			this.preds.add(v);
-		}
-	}*/
-	
+    	
 	/**
 	 * add a new edge with its value cost. If the successor exists, the weight is changed.
 	 */
 	public void addArc(Arc a1) {
 		if(a1.getFirstNode().equals(this)) { // case where the arc is out-going from "this".
 			if(!this.getArcSucc().contains(a1)) {		
-				/*if(!this.succs.contains(a1.getSecondNode())) { // If the successor was not in the list, we add it.	
-					this.addSucc(a1.getSecondNode());				
-				}*/				
 				this.arcSucc.add(a1);
 			}
 			else {
@@ -138,9 +81,6 @@ public class DirectedNode extends AbstractNode {
 		}
 		else if(a1.getSecondNode().equals(this)) { // case where the arc is in-going to "this".
 			if(!this.getArcPred().contains(a1)) {		
-				/*if(!this.preds.contains(a1.getFirstNode())) { // If the predecessor was not in the list, we add it.				
-					this.addPred(a1.getFirstNode());				
-				}*/				
 				this.arcPred.add(a1);
 			}
 			else {
--- FiT_graph_student/src/main/java/Nodes_Edges/UndirectedNode.java
+++ FiT_graph_student/src/main/java/Nodes_Edges/UndirectedNode.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 
 /**
- * Created by gsimonin on 05/01/2020.
+ * Created by gsimonin on 05/03/2025.
  */
 public class UndirectedNode extends AbstractNode {
 
@@ -13,8 +13,6 @@ public class UndirectedNode extends AbstractNode {
     // 				Class variables
     //--------------------------------------------------
 
-    //private Map<UndirectedNode, Integer> neighbours;
-	//private List<UndirectedNode> neighbours;
 	private List<Edge> incidentEdges; // List of edges incident with the node this.
 
     //--------------------------------------------------
@@ -23,7 +21,6 @@ public class UndirectedNode extends AbstractNode {
 
     public UndirectedNode(int i) {
         super(i);
-       // this.neighbours = new LinkedList<UndirectedNode>();
         this.incidentEdges = new LinkedList<Edge>();        
     }
 
@@ -32,26 +29,12 @@ public class UndirectedNode extends AbstractNode {
     // ------------------------------------------
 
     /**
-     * @return the list of neighbors of the current node this
-     */
-   /* public List<UndirectedNode> getNeighbours() {
-        return this.neighbours;
-    }*/
-
-    /**
      * @return the list of incident edges of the current node this
      */
     public List<Edge> getIncidentEdges() {
         return this.incidentEdges;
     }
 
-    
-    /**
-     * @param neigh the new list of neighbors for node this
-     */
-    /*public void setNeighbours(List<UndirectedNode> neigh) {
-		this.neighbours = neigh;
-	}*/
 
     /**
      * @return the number of neighbors of node this
@@ -59,31 +42,18 @@ public class UndirectedNode extends AbstractNode {
 	public int getNbNeigh() {
         return incidentEdges.size();
     }
-
-
-	/**
-	 * add a new neighbour if it does not exist already.
-	 */
-	/*public void addNeigh(UndirectedNode v) {
-		if(!this.neighbours.contains(v)) {
-			this.neighbours.add(v);
-		}
-	}*/
+	
 
 	/**
 	 * add a new edge with its value cost. If the neighbour exists, the weight is changed.
 	 */
 	public void addEdge(Edge e1) {
 		if(!this.getIncidentEdges().contains(e1)) {			
-			//if(e1.getFirstNode().equals(this) && !this.neighbours.contains(e1.getSecondNode())) {
 			if(e1.getFirstNode().equals(this)) {
 				this.incidentEdges.add(e1);
-				//this.addNeigh(e1.getSecondNode());				
 			}
-			//else if(e1.getSecondNode().equals(this) && !this.neighbours.contains(e1.getFirstNode())) {
 			else if(e1.getSecondNode().equals(this) ) {
 				this.incidentEdges.add(new Edge(e1.getSecondNode() ,e1.getFirstNode(),e1.getWeight()));
-				//this.addNeigh(e1.getFirstNode());
 			} 			
 		}
 		else {
